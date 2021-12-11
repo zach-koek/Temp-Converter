e728efbfe10a831e2ba26dd9b8b8d6b51e308ffc
zach-koek <Zachary.Koek@gmail.com>
2021-12-11 11:46:19 -0500

    Add project files.

---
+++ TempChange/frmTempCalc.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace TempChange
+{
+    partial class frmTempCalc
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFahrenheit = new System.Windows.Forms.Button();
+            this.btnCelsius = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFahrenheit
+            // 
+            this.btnFahrenheit.Location = new System.Drawing.Point(12, 70);
+            this.btnFahrenheit.Name = "btnFahrenheit";
+            this.btnFahrenheit.Size = new System.Drawing.Size(139, 47);
+            this.btnFahrenheit.TabIndex = 0;
+            this.btnFahrenheit.Text = "Conver to &Fahrenheit";
+            this.btnFahrenheit.UseVisualStyleBackColor = true;
+            this.btnFahrenheit.Click += new System.EventHandler(this.btnFahrenheit_Click);
+            // 
+            // btnCelsius
+            // 
+            this.btnCelsius.Location = new System.Drawing.Point(179, 70);
+            this.btnCelsius.Name = "btnCelsius";
+            this.btnCelsius.Size = new System.Drawing.Size(127, 47);
+            this.btnCelsius.TabIndex = 1;
+            this.btnCelsius.Text = "Conver to &Celsius";
+            this.btnCelsius.UseVisualStyleBackColor = true;
+            this.btnCelsius.Click += new System.EventHandler(this.btnCelsius_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(122, 41);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(81, 23);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(76, 23);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(188, 15);
+            this.lblInstructions.TabIndex = 3;
+            this.lblInstructions.Text = "Please put a valid tempature here :";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(122, 138);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(81, 23);
+            this.txtResult.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Results:";
+            // 
+            // frmTempCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 192);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCelsius);
+            this.Controls.Add(this.btnFahrenheit);
+            this.Name = "frmTempCalc";
+            this.Text = "Climate Change";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFahrenheit;
+        private System.Windows.Forms.Button btnCelsius;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TempChange/frmTempCalc.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempChange
+{
+    public partial class frmTempCalc : Form
+    {
+        //private variables only used to identify if a btn was clicked.
+        public bool ifbtnFarhrenheithClicked = false;
+        public bool ifbtnCelciusClicked = false;
+
+        public frmTempCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*When button is clicked it takes the input changes it to an int does the math equation
+          and then returns it to a string to print out*/
+         
+        private void btnFahrenheit_Click(object sender, EventArgs e)
+        {
+            ifbtnFarhrenheithClicked = true;
+            Calculations();
+        }
+
+        /*When button is clicked it takes the input changes it to an int does the math equation
+          and then returns it to a string to print out*/
+        private void btnCelsius_Click(object sender, EventArgs e)
+        {
+            ifbtnCelciusClicked = true;
+            Calculations();
+        }
+        
+        //tried to be fancy and put everything together. 
+        private void Calculations()
+        {
+            //try statement to do the equations
+            try
+            {
+                int input = Convert.ToInt32(txtInput.Text);
+                int result = 0;
+
+                //if statement that looks to see if one of the buttons were clicked.
+                if (ifbtnCelciusClicked)
+                {
+                    result = (input - 32) * 5 / 9;
+                }
+                if (ifbtnFarhrenheithClicked)
+                {
+                    result = ((input * 9) / 5) + 32;
+                }
+
+                txtResult.Text = result.ToString();
+            }
+            //all of my catch statements some I believe may be redundent but you can never be to careful when you trust humans.
+            //catch statement if you divide by 0 (impossible to throw this?)
+            catch (DivideByZeroException de)
+            {
+                MessageBox.Show(de.Message, "Congrats IDK how you did this!");
+            }
+            // catch if the int is too big (Temps never get this high)
+            catch (OverflowException oe)
+            {
+                MessageBox.Show(oe.Message, "Temperature is too high!");
+            }
+            //catch if they put a letter, symbol, or decimal instead of a int
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message, "Please use real numbers!");
+            }
+            //catch if math is done wrong.
+            catch (ArithmeticException ae)
+            {
+                MessageBox.Show(ae.Message, "Somehow you did math wrong!?");
+            }
+            //catch if something unexpected happens. Generic
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error has Occured!");
+            }
+
+
+            ResetBtn();
+
+        }
+
+        //resets button click so you dont have to retype the number.
+        //If this is a more difficult way to do this could you please let me know if there is an easier way?
+        private void ResetBtn()
+        {
+           if (ifbtnFarhrenheithClicked)
+            {
+                ifbtnFarhrenheithClicked = false;
+            }
+            if (ifbtnCelciusClicked)
+            {
+                ifbtnFarhrenheithClicked = false;
+            } 
+        } 
+    }
+}
